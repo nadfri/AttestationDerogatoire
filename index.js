a8b467964cc2ecfcd3111b94f812d384f5979c66
Nader Frigui <nadfri@gmail.com>
2020-04-17 21:16:54 +0200

    fonction bouton swtich et Div_NoUser

--- index.js
+++ index.js
@@ -12,32 +12,38 @@ let switch_position = false;
 //***************Bouton Switch User/Attestation *****************/
 bt_switch.onclick = () =>{
     if(switch_position == false)
-    {
-        field_Attest.style.display = "none";
-        field_User.style.display = "block";
+    {  
+        field_Attest.style.display = "none"; //masque fieldSet Attestation
+        field_User.style.display = "block"; //affiche fieldSet User
+        bt_switch.src ="img/list.png";     //modifie l'icone
         switch_position = true;
-        bt_switch.src ="img/list.png";
     }
     else
     {
         field_Attest.style.display = "block";
         field_User.style.display = "none";
-        switch_position = false;
         bt_switch.src ="img/contact.png";
-
+        switch_position = false;
     }
 };
 
-//**********/Désactivation du bouton new Attestation si No User***/
+//**********/Désactivation du bouton new Attestation si No User et de la div_NoUser***/
 if(tabUsers.length == 0) 
 {
-    bt_newAttest.disabled = true;
+    bt_newAttest.disabled = true; //desactive le bouton new Attestation
     bt_newAttest.src = "img/attestIcon2.png";
+
+    for(let div of document.getElementsByClassName("div_NoUser"))
+        div.style.display = "block"; //affiche les divs ayant la class div_NoUser   
 }
 else 
 {
-    bt_newAttest.display = false;
+    bt_newAttest.disabled = false;
     bt_newAttest.src = "img/attestIcon.png";
+
+    for(let div of document.getElementsByClassName("div_NoUser"))
+        div.style.display = "none"; //masque les divs ayant la class div_NoUser
+
 }
 
 //****************** */
